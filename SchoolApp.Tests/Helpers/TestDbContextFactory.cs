58b23f71931a32e33d0e9d64dbb267d729ad9f98
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-04-24 19:56:03 +0300

    Add project files.

---
+++ SchoolApp.Tests/Helpers/TestDbContextFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolApp.Tests.Helpers
+{
+    public static class TestDbContextFactory
+    {
+    }
+}
---
+++ SchoolApp/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using SchoolApp.Core;
+using SchoolApp.Models;
+using System.Linq.Expressions;
+
+namespace SchoolApp.Repositories
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<User> GetUserByUsernameAsync(String username);
+
+        Task<PaginatedResult<User>> GetUsersAsync(int pageNumber, int pageSize,
+            List<Expression<Func<User, bool>>> predicates);
+    }
+}
